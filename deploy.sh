e322e15f39ee321f78b6a6bbb0bd74caac808e01
Hafizur Rahman <hafizur.rahman@coatesgroup.com>
2019-06-19 10:13:54 +1000

    shell script to deploy the app is added.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env
+export S3_ACCESS_KEY=""
+export S3_SECRET_KEY=""
+export S3_BUCKET=""
+export S3_REGION=""
+export MAX_FILE_SIZE=1024*1024*5 # will only allow 5MB, change according to your need
+
+node ./server.js
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 AWS.config.update({
-    secretAccessKey: '',
-    accessKeyId: '',
-    region: 'ap-southeast-2'
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    region: process.env.S3_REGION
 });
 const PORT = 3001;
 
@@ -22,7 +22,7 @@ var s3 = new AWS.S3();
 var upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 's3-bucket-name',
+    bucket: process.env.S3_BUCKET,
     acl: 'authenticated-read',
     contentDisposition: 'attachment',
     serverSideEncryption: 'AES256',
@@ -30,10 +30,13 @@ var upload = multer({
       cb(null, {fieldName: file.fieldname});
     },
     key: function (req, file, cb) {
-        cb(null, file.originalname); // use original filename
-        // cb(null, Date.now().toString()); // use an alternative filename
+        cb(null, file.originalname); // to use original filename
+        // cb(null, Date.now().toString()); // to use an alternative filename
     }
-  })
+  }),
+  limits: {
+    fileSize: parseInt(process.env.MAX_FILE_SIZE) // we are allowing only 5 MB files
+  }
 });
 
 //open in browser to see upload form
@@ -43,8 +46,6 @@ app.get('/', function (req, res) {
 
 //use by upload form
 app.post('/upload', upload.array('upl', 1), function (req, res, next) {
-    console.log('\n\n\nreq = ', req);
-    // console.log('res = ', res);
     res.send('Successfully uploaded ' + req.files.length + ' files!\nUpload size ' + req.files[0].size + ' bytes.');
 });
 
